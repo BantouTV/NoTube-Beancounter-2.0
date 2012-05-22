2b536a3b332bf0b1e0c3242011ba9ddfa282f90d
Enrico Candino <enrico.candino@gmail.com>
2012-05-22 11:37:33 +0100

    synch removed

--- synch/core/src/main/java/tv/notube/synch/core/logger/LoggerException.java
+++
@@ -1,9 +0,0 @@
-package tv.notube.synch.core.logger;
-
-/**
- * put class description here
- *
- * @author Davide Palmisano ( dpalmisano@gmail.com )
- */
-public class LoggerException extends Exception {
-}
