cd691785b1b058d59e0b828a9bcf1e77a8fbc5ec
Davide Palmisano <dpalmisano@gmail.com>
2012-05-24 19:17:34 +0100

    activity store added

---
+++ activities/src/main/java/tv/notube/activities/ActivityStoreException.java
@@ -0,0 +1,9 @@
+package tv.notube.activities;
+
+/**
+ * put class description here
+ *
+ * @author Davide Palmisano ( dpalmisano@gmail.com )
+ */
+public class ActivityStoreException extends Exception {
+}
