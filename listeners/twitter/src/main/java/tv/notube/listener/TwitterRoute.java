b71eaf1093b9d0c5d4158a011eb22e21b7a4195e
Bilgin Ibryam <bibryam@gmail.com>
2012-06-18 17:05:14 +0100

    Added twitter4j's Status to TwitterTweet converter

--- listeners/twitter/src/main/java/tv/notube/listener/TwitterRoute.java
+++ listeners/twitter/src/main/java/tv/notube/listener/TwitterRoute.java
@@ -3,27 +3,54 @@ package tv.notube.listener;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.dataformat.JsonLibrary;
+
+import tv.notube.crawler.requester.request.twitter.TwitterTweet;
+import twitter4j.Status;
 
 public class TwitterRoute extends RouteBuilder {
 
     public void configure() {
 
         from("twitter://streaming/filter?type=event&keywords=test&consumerKey={{consumer.key}}&consumerSecret={{consumer.secret}}&accessToken={{access.token}}&accessTokenSecret={{access.token.secret}}")
+
+                .process(new Processor() {
+                    @Override
+                    public void process(Exchange exchange) throws Exception {
+                        Status status = exchange.getIn().getBody(Status.class);
+                        TwitterTweet twitterTweet = new TweetConverter().convert(status);
+                        exchange.getIn().setBody(twitterTweet);
+                    }
+                })
+
+                .marshal().json(JsonLibrary.Jackson)
+
+                .convertBodyTo(String.class)
+
                 .process(new Processor() {
                     @Override
                     public void process(Exchange exchange) throws Exception {
                         System.out.println("sending " + exchange.getIn().getBody());
+
+
                     }
                 })
-                .to("kestrel://{{kestrel.queue.url}}");
 
-        from("kestrel://{{kestrel.queue.url}}?concurrentConsumers=10&waitTimeMs=500")
-                .process(new Processor() {
-                    @Override
-                    public void process(Exchange exchange) throws Exception {
-                        System.out.println("received " + exchange.getIn().getBody());
-                    }
-                });
+
+        //  .unmarshal().json(JsonLibrary.Jackson, TwitterTweet.class)
+
+
+
+        //.to("file:outbox");
+                .to("kestrel://{{kestrel.queue.url}}");
+//
+//        from("kestrel://{{kestrel.queue.url}}?concurrentConsumers=10&waitTimeMs=500")
+//                .process(new Processor() {
+//                    @Override
+//                    public void process(Exchange exchange) throws Exception {
+//                        System.out.println("received " + exchange.getIn().getBody());
+//                    }
+//                });
 
     }
 }
---
+++ listeners/twitter/src/test/java/tv/notube/listener/TweetConverterTest.java
@@ -0,0 +1,82 @@
+package tv.notube.listener;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Date;
+
+import org.joda.time.DateTime;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import tv.notube.crawler.requester.request.twitter.TwitterTweet;
+import twitter4j.HashtagEntity;
+import twitter4j.Status;
+import twitter4j.URLEntity;
+import twitter4j.User;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.testng.AssertJUnit.assertTrue;
+
+public class TweetConverterTest {
+
+    private final TweetConverter underTest = new TweetConverter();
+    private final Status status = mock(Status.class);
+    private final User user = mock(User.class);
+    private final URLEntity urlEntity = mock(URLEntity.class);
+    private URLEntity[] urlEntities = new URLEntity[] {urlEntity};
+    private final HashtagEntity hashtagEntity = mock(HashtagEntity.class);
+    private final HashtagEntity[] hashtagEntities = new HashtagEntity[] {hashtagEntity};
+
+    @BeforeMethod
+    protected void setUp() throws Exception {
+        when(status.getUser()).thenReturn(user);
+        when(status.getURLEntities()).thenReturn(urlEntities);
+        when(status.getHashtagEntities()).thenReturn(hashtagEntities);
+    }
+
+    @Test
+    public void convertsCreatedDate() {
+        Date createdDate = new Date();
+        when(status.getCreatedAt()).thenReturn(createdDate);
+
+        TwitterTweet twitterTweet = underTest.convert(status);
+        assertThat(twitterTweet.getCreatedAt(), is(new DateTime(createdDate.getTime())));
+    }
+
+    @Test
+    public void convertsScreenName() {
+        String userName = "Bob";
+        when(user.getScreenName()).thenReturn(userName);
+
+        TwitterTweet twitterTweet = underTest.convert(status);
+        assertThat(twitterTweet.getUsername(), is(userName));
+    }
+
+    @Test
+    public void convertsTweetUrl() {
+        String userName = "Bob";
+        when(user.getScreenName()).thenReturn(userName);
+
+        TwitterTweet twitterTweet = underTest.convert(status);
+        assertTrue(twitterTweet.getUrl().toExternalForm().contains(userName));
+    }
+
+    @Test
+    public void convertsMentionedUrls() throws MalformedURLException {
+        URL url = new URL("http://java.sun.com/index.html");
+        when(urlEntity.getExpandedURL()).thenReturn(url);
+        TwitterTweet twitterTweet = underTest.convert(status);
+        assertThat(twitterTweet.getMentionedUrls().get(0), is(url));
+    }
+
+    @Test
+    public void convertsHashTags() {
+        String hashtag = "bean counter rocks!";
+        when(hashtagEntity.getText()).thenReturn(hashtag);
+        TwitterTweet twitterTweet = underTest.convert(status);
+        assertThat(twitterTweet.getHashTags().iterator().next(), is(hashtag));
+    }
+}
