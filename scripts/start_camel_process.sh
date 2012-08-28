d3c1879e8c42fc76d6c6abc1f705c53f0c31841f
Bilgin Ibryam <bibryam@gmail.com>
2012-08-28 16:13:41 +0100

    Move stage.sh to scripts folder and added other scripts for the service we use

--- scripts/start_camel_process.sh
+++ scripts/start_camel_process.sh
@@ -3,7 +3,7 @@
 (
     echo "Starting " + $1 + $2 "..." >&2
 
-    nohup java -jar ../processes/$1-$2-SNAPSHOT-jar-with-dependencies.jar -jndiProperties /guicejndi.properties > /dev/null;
+    nohup java -jar $1-$2-SNAPSHOT-jar-with-dependencies.jar -jndiProperties /guicejndi.properties > /dev/null;
 
 ) &
 
