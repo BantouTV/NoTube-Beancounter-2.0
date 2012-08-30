2de2a3071438aa4bccb6d9387affb2e068c97ece
Enrico Candino <enrico.candino@gmail.com>
2012-08-30 15:18:34 +0200

    added test for TwitterPublisher

--- publishers/twitter-publisher/src/main/java/io/beancounter/publisher/twitter/TwitterPublisherModule.java
+++ publishers/twitter-publisher/src/main/java/io/beancounter/publisher/twitter/TwitterPublisherModule.java
@@ -4,6 +4,8 @@ import com.google.inject.Provides;
 import org.apache.camel.component.properties.PropertiesComponent;
 import org.apache.camel.guice.CamelModuleWithMatchingRoutes;
 import org.guiceyfruit.jndi.JndiBind;
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
 
 /**
  *
@@ -14,6 +16,8 @@ public class TwitterPublisherModule extends CamelModuleWithMatchingRoutes {
     @Override
     protected void configure() {
         super.configure();
+        TwitterFactory factory = new TwitterFactory();
+        bind(Twitter.class).toInstance(factory.getInstance());
         bind(TwitterPublisherRoute.class);
     }
 
