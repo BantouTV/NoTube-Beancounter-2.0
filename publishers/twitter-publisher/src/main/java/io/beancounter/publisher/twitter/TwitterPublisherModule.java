6a4ee5986b674b05b4a1810f2283f07e174311da
Enrico Candino <enrico.candino@gmail.com>
2012-09-05 17:47:39 +0200

    guiced Twitter4j instantiation

--- publishers/twitter-publisher/src/main/java/io/beancounter/publisher/twitter/TwitterPublisherModule.java
+++ publishers/twitter-publisher/src/main/java/io/beancounter/publisher/twitter/TwitterPublisherModule.java
@@ -1,12 +1,16 @@
 package io.beancounter.publisher.twitter;
 
 import com.google.inject.Provides;
-import com.google.inject.Singleton;
+import io.beancounter.commons.helper.PropertiesHelper;
+import io.beancounter.commons.model.Service;
+import io.beancounter.usermanager.services.auth.DefaultServiceAuthorizationManager;
 import org.apache.camel.component.properties.PropertiesComponent;
 import org.apache.camel.guice.CamelModuleWithMatchingRoutes;
 import org.guiceyfruit.jndi.JndiBind;
 import twitter4j.Twitter;
 import twitter4j.TwitterFactory;
+
+import java.util.Properties;
 
 /**
  *
@@ -17,8 +21,15 @@ public class TwitterPublisherModule extends CamelModuleWithMatchingRoutes {
     @Override
     protected void configure() {
         super.configure();
+
+        Properties properties = PropertiesHelper.readFromClasspath("/beancounter.properties");
+        Service service = DefaultServiceAuthorizationManager.buildService("twitter", properties);
+
         TwitterFactory factory = new TwitterFactory();
-        bind(Twitter.class).toInstance(factory.getInstance());
+        Twitter twitter = factory.getInstance();
+        twitter.setOAuthConsumer(service.getApikey(), service.getSecret());
+        bind(Twitter.class).toInstance(twitter);
+
         bind(TwitterPublisher.class);
         bind(TwitterPublisherRoute.class);
     }
