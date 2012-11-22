8b112c870f14b117fe89ca1094b8be265ce0d466
Davide Palmisano <dpalmisano@gmail.com>
2012-11-22 12:35:59 +0000

    minor

--- storm/analyses/src/main/java/io/beancounter/storm/analyses/DebateAnalysesTopology.java
+++ storm/analyses/src/main/java/io/beancounter/storm/analyses/DebateAnalysesTopology.java
@@ -27,9 +27,12 @@ public class DebateAnalysesTopology {
         JedisPoolConfigSerializable config = new JedisPoolConfigSerializable();
         config.setMaxIdle(16);
         config.setMaxActive(16);
-        builder.setBolt("tweets-count", new MentionCountBolt(config, "46.4.89.183", "london", "shoreditch", "BBC", "tube"), 1).shuffleGrouping("tweets");
-        builder.setBolt("to-kestrel", new KestrelBolt("46.4.89.183", 2229, "mentions"), 1).shuffleGrouping("tweets-count");
-        builder.setBolt("storage", new RedisBolt(config, "46.4.89.183", false), 1).shuffleGrouping("tweets-count");
+        builder.setBolt("mentions", new MentionCountBolt(config, "46.4.89.183", "london", "shoreditch", "BBC", "tube"), 1).shuffleGrouping("tweets");
+        builder.setBolt("unique-users", new UniqueUsersCountBolt(config, "46.4.89.183"), 1).shuffleGrouping("tweets");
+        builder.setBolt("mentions-to-kestrel", new KestrelBolt("46.4.89.183", 2229, "mentions"), 1).shuffleGrouping("mentions");
+        builder.setBolt("unique-to-kestrel", new KestrelBolt("46.4.89.183", 2229, "unique"), 1).shuffleGrouping("unique-users");
+        builder.setBolt("mentions-storage", new RedisBolt(config, "46.4.89.183", false), 1).shuffleGrouping("mentions");
+        builder.setBolt("unique-storage", new RedisBolt(config, "46.4.89.183", false), 1).shuffleGrouping("unique-users");
         Config conf = new Config();
         conf.setDebug(false);
         conf.setNumWorkers(1);
